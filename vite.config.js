f00e5903303f96bd141ce4c4cfdb24cbd58f0217
Hthakk <1thakkarhar@hdsb.ca>
2025-05-12 21:00:41 -0400

    Pages!!

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-base: process.env.VITE_BASE_PATH || "/todo-app-1/",
-  build: {
-    rollupOptions: {
-      input: 'index.html', // Ensure this points to the correct entry file
-    },
-  },
-
-
-})
+  base: '/todo-app-1/', // Replace 'your-repo-name' with your GitHub repository name
+});
 
