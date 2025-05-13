944f40c12a74f46072b2ce5df90f68d1114afd16
Hthakk <1thakkarhar@hdsb.ca>
2025-05-12 20:31:43 -0400

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH ||"/todo-app-1"
+base: process.env.VITE_BASE_PATH || "/todo-app-1",
+
+
+root: path.resolve(__dirname, 'public'), // Adjust the path as needed
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html')
+    }
+  }
+
 
 })
 
